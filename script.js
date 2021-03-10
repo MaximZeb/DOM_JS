d689368e74970b7a19349ed6a7deeacc22dc1b7a
Maxim Zebzeev <maksimzeb@yandex.ru>
2021-03-10 20:13:22 +0500

    added div and attribute in tag body

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     let div = document.createElement('div');
+    // div parrent
     div.innerText = '1 div';
+    div.setAttribute('style', 'color: white; display:grid; max-width: 320px;  min-width: 1000px; margin: 0 auto;');
     document.body.appendChild(div);
 
-
+    // body
+    document.body.setAttribute('style', 'background: url(https://images.wallpaperscraft.ru/image/planeta_kratery_kosmos_svet_61032_1600x900.jpg) no-repeat fixed; background-size: cover;)');
+    
 
 })
